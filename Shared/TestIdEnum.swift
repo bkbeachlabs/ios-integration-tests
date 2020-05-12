4123e83328b0037cb9a60e1ebaaec58c253cd4dc
Andrew King <bkbeachlabs@gmail.com>
2020-05-12 09:30:18 -0400

    import XCTest into the app by referencing framework from Xcode directory. Also a bit of cleanup and moving files

--- Shared/TestIdEnum.swift
+++ Shared/TestIdEnum.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+/// This enum is shared between the test app and its UI Tests. This allows us to use the same
+/// constants to pick the test as to run the test.
 enum TestId: String {
     case doesSomethingRight
     case doesAnotherThing
